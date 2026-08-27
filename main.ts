bc356670e8f8f44d5bb5dca1205b91c143aab76a
ryuusouki <56426191+ryuusouki@users.noreply.github.com>
2026-08-27 18:18:44 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -141,7 +141,7 @@ namespace mecanumRobotV2 {
     //% group="General" weight=96
     //% block="Initialize robot"
     export function initializeRobot() {
-        irRemote.connectInfrared(DigitalPin.P0)
+        irRemote.connectInfrared(DigitalPin.P9)
         led.enable(false)
     }
 
